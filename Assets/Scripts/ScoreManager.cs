d8fe6f94a8ecd5aead04772efed02deb65d86482
jwlo44 <juliarochelle2@gmail.com>
2017-04-08 14:54:51 -0400

    set up score manager with timer

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : UnitySingleton<ScoreManager> {
+    [Tooltip("The starting time in seconds for the game timer.")]
+    public int MAX_TIME_VALUE =  100;
+
+    [Tooltip("The text that will show the game timer.")]
+    public Text timerText;
+    private int timer;
+    public int Timer
+    {
+        get { return timer; }
+        private set
+        {
+            timer = value;
+            if (timerText != null)
+            {
+                timerText.text = "Time: " + timer.ToString();
+            }
+        }
+    }
+
+    [Tooltip("The text that will show the game score.")]
+    public Text scoreText;
+    private int score;
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        private set
+        {
+            score = value;
+            if (scoreText != null)
+            {
+                scoreText.text = "Score: " + score.ToString();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        StopAllCoroutines();
+        Score = 0;
+        Timer = MAX_TIME_VALUE;
+    }
+
+	void Start () {
+        Reset();
+        StartCoroutine(TimerCoroutine());
+	}
+
+    private IEnumerator TimerCoroutine()
+    {
+        while(Timer > 0)
+        {
+            Timer--;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void LosePoints(int pointsToLose)
+    {
+        Score -= pointsToLose;
+    }
+
+    public void GainPoints(int pointsToGain)
+    {
+        Score += pointsToGain;
+    }
+}
