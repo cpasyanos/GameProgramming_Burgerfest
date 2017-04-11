4fa62bdff5b2e2343287de43bd2b7e07fa2196a5
cpasyanos <pasyanos.c@husky.neu.edu>
2017-04-10 22:18:05 -0400

    for now empty finger movement controller

---
+++ Assets/Scripts/FingerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerMovement : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+      // pinky
+		if (Input.GetKeyDown(KeyCode.A))
+      {
+         Debug.Log("Clench pinky");
+      }
+
+      // ring finger
+      if (Input.GetKeyDown(KeyCode.W))
+      {
+
+      }
+
+      // middle finger
+      if (Input.GetKeyDown(KeyCode.E))
+      {
+
+      }
+
+      // index finger
+      if (Input.GetKeyDown(KeyCode.R))
+      {
+
+      }
+
+      // thumb
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+
+      }
+	}
+}
