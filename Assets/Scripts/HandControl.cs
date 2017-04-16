952923475ad57fe525b5dcb1cde077b9a15c692b
ejpenniman <erica.p.78@gmail.com>
2017-04-16 13:46:39 -0400

    Made PR changes: Hand now moving with AddForce.

--- Assets/Scripts/HandControl.cs
+++ Assets/Scripts/HandControl.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 
 public class HandControl : MonoBehaviour
 {
+	private Rigidbody rigidbody;
+
 	public float speed = 10.0f;
 	public float turnSpeed = 360.0f;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
-
 		var scrollVal = Input.GetAxis("Mouse ScrollWheel");
-	
+
 		// Scroll down to lower hand
 		if (scrollVal < 0f)
 		{
 			Vector3 position = this.transform.position;
-			position.y -= speed*Time.deltaTime;
-			this.transform.position = position;
+			rigidbody.AddForce (0, position.y * -250, 0);
 		}
-
+	
 		// Scroll up to lower hand
 		if (scrollVal > 0f)
 		{
 			Vector3 position = this.transform.position;
-			position.y += speed*Time.deltaTime;
-			this.transform.position = position;
+			rigidbody.AddForce (0, position.y * 250, 0);
 		}
+
+		// Stops rigidbody movement if no scroll wheel input
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
 
 		// Press and hold left mouse button down to turn hand counterclockwise
 		if (Input.GetKey (KeyCode.Mouse0)) 
