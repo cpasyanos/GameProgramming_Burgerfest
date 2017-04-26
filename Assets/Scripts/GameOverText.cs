c9a9032183a18eb2c6fb0b6f0369f4ddc10dced6
cpasyanos <pasyanos.c@husky.neu.edu>
2017-04-26 15:53:07 -0400

    Added game over screen and score script

---
+++ Assets/Scripts/GameOverText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverText : MonoBehaviour {
+
+   public string getScoreText()
+   {
+      return "You made " + 7 +" burgers!";
+   }
+}
