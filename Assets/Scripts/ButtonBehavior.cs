d20265d9459c9d0b411c9e8d85500b9c470738b6
cpasyanos <pasyanos.c@husky.neu.edu>
2017-04-27 10:36:50 -0400

    added quit button and updated gameover prefab

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour {
+
+   public void quitButton()
+   {
+      Debug.Log("quitted");
+      Application.Quit();
+   }
+
+}
